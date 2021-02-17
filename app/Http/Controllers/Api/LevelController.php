82e6c30e930d50751ed10af2875bd95f9bb7ed4d
samankassou <samankassoufoulla@gmail.com>
2021-02-17 13:54:06 +0100

    Create Level and AcademicYear resources

--- app/Http/Controllers/Api/LevelController.php
+++ app/Http/Controllers/Api/LevelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\LevelResource;
 use App\Models\Level;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class LevelController extends Controller
      */
     public function index()
     {
-        //
+        return LevelResource::collection(Level::with(['cycle', 'classrooms'])->get());
     }
 
     /**
