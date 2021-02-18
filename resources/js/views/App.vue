1fcc5a84678c5b1940d82b6adda4a79836392acd
samankassou <samankassoufoulla@gmail.com>
2021-02-18 10:11:43 +0100

    Setup vuejs for the front part of the project

---
+++ resources/js/views/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      {{ message }}
+  </div>
+</template>
+
+<script>
+const default_layout = "default";
+export default {
+    data() {
+        return {
+            message: "Hello world"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
